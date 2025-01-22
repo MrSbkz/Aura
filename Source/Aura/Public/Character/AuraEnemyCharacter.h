488779499bf06652c3dfcc2d7d1decca680acf1e
Pavel <Krishin>
2025-01-22 09:14:57 +0100

    73 - Create ICombatInterface, add Level value

--- Source/Aura/Public/Character/AuraEnemyCharacter.h
+++ Source/Aura/Public/Character/AuraEnemyCharacter.h
@@ -19,8 +19,15 @@ public:
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
 	/** end Enemy interface */
+	
+	/** Combat interface */
+	virtual int32 GetPlayerLevel() override;
+	/** end Combat interface */
 
 protected:
 	virtual void BeginPlay() override;
 	virtual void InitAbilityActorInfo() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Character Class Defaults")
+	int32 Level = 1;
 };
