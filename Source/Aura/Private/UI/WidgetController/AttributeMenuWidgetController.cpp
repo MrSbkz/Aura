328af123a2fff997974291f9852d09c1374326ee
Pavel <Krishin>
2025-01-26 13:47:52 +0100

    90 - Create AttributeMenuWidgetController

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,11 @@
+// Copyright PK
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+}
