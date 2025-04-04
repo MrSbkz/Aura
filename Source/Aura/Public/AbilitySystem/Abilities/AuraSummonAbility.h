588be1a0abbbca752b2205ca3559f10bdf345776
MrSbkz <48620291+MrSbkz@users.noreply.github.com>
2025-04-04 11:50:54 +0200

    219 - Calculate spawn locations (#175)

---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraSummonAbility.h
@@ -0,0 +1,35 @@
+// Copyright PK
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraGameplayAbility.h"
+#include "AuraSummonAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraSummonAbility : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	TArray<FVector> GetSpawnLocations();
+
+	UPROPERTY(EditDefaultsOnly, Category="Summonning")
+	int32 NumMinions = 5;
+
+	UPROPERTY(EditDefaultsOnly, Category="Summonning")
+	TArray<TSubclassOf<APawn>> MinionClasses;
+
+	UPROPERTY(EditDefaultsOnly, Category="Summonning")
+	float MinSpawnDistance = 150.f;
+
+	UPROPERTY(EditDefaultsOnly, Category="Summonning")
+	float MaxSpawnDistance = 400.f;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Summonning")
+	float SpawnSpread = 90.f;
+};
