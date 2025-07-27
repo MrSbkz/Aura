23743ed6a8660191ada044a4d02e62a19a23487c
MrSbkz <48620291+MrSbkz@users.noreply.github.com>
2025-07-27 14:48:58 +0200

    282 - Implement showing abilities in the Spell Tree (#234)

--- Source/Aura/Public/AbilitySystem/Data/AbilityInfo.h
+++ Source/Aura/Public/AbilitySystem/Data/AbilityInfo.h
@@ -16,6 +16,9 @@ struct FAuraAbilityInfo
 	FGameplayTag AbilityTag = FGameplayTag();
 
 	UPROPERTY(BlueprintReadOnly)
+	FGameplayTag StatusTag = FGameplayTag();
+
+	UPROPERTY(BlueprintReadOnly)
 	FGameplayTag InputTag = FGameplayTag();
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
