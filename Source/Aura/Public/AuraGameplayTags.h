1f42146ed25608183f3fba86b15b1b07656491ac
Pavel <mr.sobakaz@gmail.com>
2025-07-29 16:05:45 +0200

    289 - Implement enabling/disabling SpellPoints and Equip buttons

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -66,6 +66,7 @@ public:
 	FGameplayTag Effects_HitReact;
 
 	// Ability
+	FGameplayTag Abilities_None;
 	FGameplayTag Abilities_Attack;
 	FGameplayTag Abilities_Summon;
 	FGameplayTag Abilities_Fire_FireBolt;
