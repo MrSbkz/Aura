01c5e94c30f7a3ac3c3f5b5bb3c0854d6040bdb8
Pavel <mr.sobakaz@gmail.com>
2025-08-04 12:07:55 +0200

    313 - Add Death Impulse parameters to the AuraGameplayEffectContext

--- Source/Aura/Private/Actor/AuraProjectile.cpp
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -85,6 +85,8 @@ void AAuraProjectile::OnSphereOverlap(
 	{
 		if (UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(OtherActor))
 		{
+			const FVector DeathImpulse = GetActorForwardVector() * DamageEffectParams.DeathImpulseMagnitude;
+			DamageEffectParams.DeathImpulse = DeathImpulse;
 			DamageEffectParams.TargetAbilitySystemComponent = TargetASC;
 			UAuraAbilitySystemLibrary::ApplyDamageEffect(DamageEffectParams);
 		}
