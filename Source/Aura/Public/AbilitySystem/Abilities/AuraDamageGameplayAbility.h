fea2823c4e3af4b54d432f0dd9067af920dd1fbf
Pavel <mr.sobakaz@gmail.com>
2025-08-08 13:06:21 +0200

    332 - Apply Electrocute cost and damage

--- Source/Aura/Public/AbilitySystem/Abilities/AuraDamageGameplayAbility.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraDamageGameplayAbility.h
@@ -23,6 +23,9 @@ public:
 	UFUNCTION(BlueprintPure)
 	FDamageEffectParams MakeDamageEffectParamsFromClassDefaults(AActor* TargetActor = nullptr) const;
 
+	UFUNCTION(BlueprintPure)
+	float GetDamageAtLevel() const;
+	
 protected:
 	UFUNCTION(BlueprintPure)
 	static FTaggedMontage GetRandomMontageFromArray(const TArray<FTaggedMontage>& TaggedMontages);
