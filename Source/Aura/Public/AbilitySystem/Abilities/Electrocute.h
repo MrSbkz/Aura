dcf01fd0db121310bb4556e9ce6eb70273c009cd
MrSbkz <48620291+MrSbkz@users.noreply.github.com>
2025-09-06 11:24:58 +0200

    361 - Add descriptions for Lighting and ArcaneShards abilities (#313)

---
+++ Source/Aura/Public/AbilitySystem/Abilities/Electrocute.h
@@ -0,0 +1,20 @@
+// Copyright PK
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraBeamSpell.h"
+#include "Electrocute.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UElectrocute : public UAuraBeamSpell
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual FString GetDescription(int32 Level) override;
+	virtual FString GetNextLevelDescription(int32 Level) override;
+};
