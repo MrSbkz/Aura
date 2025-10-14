2e2ec50a93e5ebca78f6272b8504555f69072c9a
MrSbkz <48620291+MrSbkz@users.noreply.github.com>
2025-10-14 11:02:37 +0200

    383 - Set view model for load slot widgets (#332)

--- Source/Aura/Public/UI/ViewModel/MVVM_LoadScreen.h
+++ Source/Aura/Public/UI/ViewModel/MVVM_LoadScreen.h
@@ -6,6 +6,7 @@
 #include "MVVMViewModelBase.h"
 #include "MVVM_LoadScreen.generated.h"
 
+class UMVVM_LoadSlot;
 /**
  * 
  */
@@ -14,4 +15,25 @@ class AURA_API UMVVM_LoadScreen : public UMVVMViewModelBase
 {
 	GENERATED_BODY()
 	
+public:
+	void InitializeLoadSlots();
+
+	UFUNCTION(BlueprintPure)
+	UMVVM_LoadSlot* GetLoadSlotViewModelByIndex(const int32 Index) const;
+	
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UMVVM_LoadSlot> LoadSlotViewModelClass;
+
+private:
+	UPROPERTY()
+	TMap<int32, UMVVM_LoadSlot*> LoadSlots;
+
+	UPROPERTY()
+	TObjectPtr<UMVVM_LoadSlot> LoadSlot_0;
+
+	UPROPERTY()
+	TObjectPtr<UMVVM_LoadSlot> LoadSlot_1;
+
+	UPROPERTY()
+	TObjectPtr<UMVVM_LoadSlot> LoadSlot_2;
 };
