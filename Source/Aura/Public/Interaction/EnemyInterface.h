b8aa656d3a09e23d1a764f9528797243d47656a1
MrSbkz <48620291+MrSbkz@users.noreply.github.com>
2025-11-02 11:27:15 +0100

    411 - Create Highlight interface (#359)

--- Source/Aura/Public/Interaction/EnemyInterface.h
+++ Source/Aura/Public/Interaction/EnemyInterface.h
@@ -17,9 +17,6 @@ class AURA_API IEnemyInterface
 	GENERATED_BODY()
 
 public:
-	virtual void HighlightActor() = 0;
-	virtual void UnHighlightActor() = 0;
-
 	// Called in the BTT_Attack task
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	void SetCombatTarget(AActor* InCombatTarget);
